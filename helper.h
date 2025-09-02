5329b142af6cb5282d5bc89ebc1f5837f5db271b
Rafael Eberhardt Sarate <rafaelsarate@gmail.com>
2025-09-02 16:05:30 -0300

    Compare different initial conditions

--- helper.h
+++ helper.h
@@ -1,7 +1,15 @@
 #ifndef HELPER_H
 #define HELPER_H
 
-void generate_gnuplot_script(const char *datafile, const char *output_gif);
+// Array of size 3
+typedef struct {
+    double x;
+    double y;
+    double z;
+} Vec3;
+
+Vec3 get_initial_conditions(const char *datafile);
+void generate_gnuplot_script(const char *datafileA, const char *datafileB, const char *datafileC, const char *output_gif);
 void generate_gnuplot_gif_script(const char *datafile, const char *output_gif, int numframes, int total_points);
 
 #endif
